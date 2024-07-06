10e94edf5b6e6817798eda7cb76e1cb199fa0cf9
林信欢 <2021469084@qq.com>
2024-07-06 14:17:01 +0800

    feat 添加新闻数据接口/api/news

---
+++ home/src/api/news/index.js
@@ -0,0 +1,32 @@
+// 新闻API
+import request from "@/utils/request";
+
+class News {
+    static async getNewsById(id) {
+        try {
+            const response = await request({
+                url: "/news/getNewsById",
+                method: "get",
+                params: {
+                    id: id
+                }
+            })
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+    static async getNewsList() {
+        try {
+            const response = await request({
+                url: "/news/getNewsList",
+                method: "get",
+            })
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default News;
