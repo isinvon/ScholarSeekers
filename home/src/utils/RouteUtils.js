33e449084a8f11a57a53df9a6ba88a14ac9ab059
林信欢 <2021469084@qq.com>
2024-07-08 01:20:02 +0800

    feat RouteUtils.js路由工具类

---
+++ home/src/utils/RouteUtils.js
@@ -0,0 +1,31 @@
+// url工具类
+class RouteUtils {
+    // 返回上一个页面
+    static goBackToPreviousPage() {
+        window.history.back();
+    }
+    // 跳转到首页
+    static goToHomePage() {
+        window.location.href = '/home';
+    }
+    // 跳转到指定页面
+    static goToPage(url) {
+        window.location.href = url;
+    }
+    // 跳转到指定页面并刷新
+    static goToPageAndRefresh(url) {
+        window.location.href = url;
+        window.location.reload();
+    }
+    // 只刷新当前页面
+    static refreshCurrentPage() {
+        window.location.reload();
+    }
+    // 清除路由历史
+    static clearHistory() {
+        window.history.replaceState(null, null, window.location.href);
+    }
+}
+
+export default RouteUtils;
+
