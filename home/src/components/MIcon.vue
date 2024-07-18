2e3b71b668941f57914c02afb9184dcf55ab9e41
林信欢 <2021469084@qq.com>
2024-07-19 05:21:19 +0800

    feat: 定制Icon图标组件

---
+++ home/src/components/MIcon.vue
@@ -0,0 +1,35 @@
+<script setup lang="js">
+import {Icon} from "@iconify/vue";
+
+defineOptions({
+  name: "MIcon",
+});
+const props = defineProps({
+  icon: {
+    type: [String],
+    default: "",
+  },
+  size: {
+    typer: [String, Number],
+    default: 32,
+  },
+  color: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <div class="rounded-full flex items-center">
+    <icon :icon="icon" class="icon-size" :width="size" :height="size"/>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.icon-size {
+@apply shadow-sm rounded-full;
+  color: v-bind(color);
+  object-fit: fill;
+}
+</style>
---
+++ home/src/menuData/icon.js
@@ -0,0 +1,12 @@
+// 此枚举类的图标来源: https://icones.js.org/, 安装教程: npm install @iconify/vue 使用: import {Icon} from "@iconify/vue";
+const icon =  {
+    // 不喜欢
+    dislike: "icon-park-outline:thumbs-down",
+    // 喜欢
+    like: "icon-park-outline:thumbs-up",
+    // 心碎
+    brokenHeart: "icon-park-outline:dislike",
+    // 爱心
+    heart: "icon-park-outline:like",
+}
+export default icon;
