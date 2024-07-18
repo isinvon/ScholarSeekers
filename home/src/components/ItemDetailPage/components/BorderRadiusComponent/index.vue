3b07be69ca11a22d6b823dc4ecd43f077578188f
林信欢 <2021469084@qq.com>
2024-07-19 05:39:04 +0800

    feat: 圆角边框组件

---
+++ home/src/components/ItemDetailPage/components/BorderRadiusComponent/index.vue
@@ -0,0 +1,71 @@
+<!--圆角边框组件-->
+<template>
+  <div
+      class="radius"
+      :style="{
+          borderRadius: radiusGroup.type
+            ? `var(--el-border-radius-${radiusGroup.type})`
+            : '',
+        }"
+  />
+</template>
+
+<script lang="js" setup>
+import {ref} from 'vue'
+defineOptions({
+  name: 'BorderRadiusComponent',
+})
+const radiusGroup = ref(
+  {
+    name: 'Round Radius',
+    type: 'round',
+  },
+)
+// const radiusGroup = ref([
+//   // {
+//   //   name: 'No Radius',
+//   //   type: '',
+//   // },
+//   // {
+//   //   name: 'Small Radius',
+//   //   type: 'small',
+//   // },
+//   // {
+//   //   name: 'Large Radius',
+//   //   type: 'base',
+//   // },
+//   {
+//     name: 'Round Radius',
+//     type: 'round',
+//   },
+// ])
+
+const getValue = (type) => {
+  const getCssVarValue = (prefix, type) =>
+      getComputedStyle(document.documentElement).getPropertyValue(
+          `--el-${prefix}-${type}`
+      )
+  return getCssVarValue('border-radius', type)
+}
+</script>
+<style scoped>
+.demo-radius .title {
+  color: var(--el-text-color-regular);
+  font-size: 18px;
+  margin: 10px 0;
+}
+
+.demo-radius .value {
+  color: var(--el-text-color-primary);
+  font-size: 16px;
+  margin: 10px 0;
+}
+
+.demo-radius .radius {
+  height: 40px;
+  width: 70%;
+  border: 1px solid var(--el-border-color);
+  border-radius: 0;
+  margin-top: 20px;
+}
+</style>
